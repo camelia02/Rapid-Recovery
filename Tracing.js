4455ad479fcb3ff02b7356dfe189cbecef0c2722
Camelia Ariana <67178761+camelia02@users.noreply.github.com>
2022-11-09 12:29:42 -0700

    Update Tracing.js

--- Tracing.js
+++ Tracing.js
@@ -1,16 +1,25 @@
-let path = [];
-let painting = false;
-let current;
-let previous;
+let bg;
+let home;
+let button;
 
 function setup() {
   createCanvas(960, 720);
+  bg = loadImage('tracing.png');
+  button = createButton('home');
+  button.position(900, 10);
+  button.mousePressed(gotolink_act1);  
 }
 
 function draw() {
- stroke("black");
+  background(bg);
+  stroke("black");
   if (mouseIsPressed === true) {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
 
+
+
+function gotolink_act1(){
+  window.open('https://editor.p5js.org/manas__1404/sketches/48Gid6pnb')
+}
