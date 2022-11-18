dae37426aa2271b416fa0ae0ded253404878c6bb
Camelia Ariana <67178761+camelia02@users.noreply.github.com>
2022-11-17 20:02:41 -0700

    Update Tracing.js

--- Tracing.js
+++ Tracing.js
@@ -1,24 +1,60 @@
 let bg;
 let home;
-let button;
+let information;
+let drawnLines = [];
+let attempt = 10;
+let locked = false;
 
 function setup() {
   createCanvas(960, 720);
   bg = loadImage('tracing.png');
-  button = createButton('home');
-  button.position(900, 10);
-  button.mousePressed(gotolink_act1);  
+  home = createButton('home');
+  home.position(900, 10);
+  home.mousePressed(gotolink_act1);  
+  information = createButton('information');
+  information.position(868, 35);
+  
 }
 
 function draw() {
   background(bg);
   stroke("black");
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  if(attempt > 0){
+  drawnLines.forEach(drawnLine => {
+    line(
+      drawnLine.mouseX,
+      drawnLine.mouseY,
+      drawnLine.pmouseX,
+      drawnLine.pmouseY);
+  });
   }
+  
+  console.log(attempt);
 }
 
+function mouseReleased(){
+  locked = false;
+  attempt--;
+}
 
+function mouseDragged() {
+  if(mouseX > 210 && mouseX < 720 && mouseY > 60 && mouseY <660){
+  playing();
+  }
+  else{
+    locked = true;
+  }
+}
+function playing(){
+  if(!locked){
+   drawnLines.push({
+    mouseX: mouseX,
+    mouseY: mouseY,
+    pmouseX: pmouseX,
+    pmouseY: pmouseY,
+  });
+  }
+}
 
 function gotolink_act1(){
   window.open('https://editor.p5js.org/manas__1404/sketches/48Gid6pnb')
