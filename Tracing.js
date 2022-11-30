6e86a65147cc68d390bb535b2b602a8f7530f825
Camelia Ariana <67178761+camelia02@users.noreply.github.com>
2022-11-30 11:25:39 -0700

    Update Tracing.js

--- Tracing.js
+++ Tracing.js
@@ -1,27 +1,37 @@
 let bg;
 let home;
-let w;
+let good;
+let bad;
 let information;
 let retry;
+let drawnLines = [];
 let cLines = [];
 let wLines = [];
 let target = [];
 let attempt = 10;
 let score = 0;
 let locked = false;
+let finnish = false;
+let begin = false;
+let mood;
 
-//add circle,
+
 function restart(){
-  attempt =11;
+  attempt = 11;
   score = 0;
   drawnLines.length = 0;
-  setup();
+  cLines.length  = 0;
+  wLines.length = 0;
+  finnish = false;
+  locked = false;
+  begin = false;
 }
 
 function setup() {
   createCanvas(960, 720);
   bg = loadImage('tracing.jpg');
-  w = loadSound('wsound.wav')
+  good = loadSound('calm.mp3');
+  bad = loadSound('creepy.mp3');
   
   home = createButton('home');
   home.position(900, 10);
@@ -33,15 +43,12 @@ function setup() {
   
   retry = createButton('retry');
   retry.position(908, 60);
-  retry.mousePressed(restart);
-  
- 
-  
-
+  retry.mousePressed(gotolink_tracing);
 }
 
 function draw() {
   background(bg);
+  //draw star
   stroke("black");
   stroke(1);
   strokeWeight(1);
@@ -53,7 +60,8 @@ function draw() {
   setLineDash([5, 5]);
   star(0, 0, 30, 70);
   pop();
-  
+    
+  //draw lines
   if(attempt > 0){
     cLines.forEach(drawnLine => {
     strokeWeight(10);
@@ -64,6 +72,7 @@ function draw() {
       drawnLine.pmouseX,
       drawnLine.pmouseY);
   });
+  
     wLines.forEach(drawnLine => {
     strokeWeight(10);
     stroke("red");
@@ -73,31 +82,69 @@ function draw() {
       drawnLine.pmouseX,
       drawnLine.pmouseY);
   });
+    
   }
-
+  end();
   Score();
   att();
+  if(finnish){
+  endScreen();
+  }
+  
+  
+}
+
+function DJ(){
+    if(mood){
+      bad.pause();
+      if(!good.isPlaying()){
+      good.play();
+      }
+    }
+    else if(!mood){
+      good.pause();
+      if(!bad.isPlaying()){
+      bad.play();
+      }
+    }
+  
+
 }
 
 function mouseReleased(){
   locked = false;
   attempt--;
+  good.pause();
+  bad.pause();
+  
   if(attempt <=0){
+    finnish = true;
     locked = true;
   }
 }
 
 function mouseDragged() {
+  DJ();
   if(mouseX > 210 && mouseX < 720 && mouseY > 60 && mouseY <660){
   playing(mouseX, mouseY);
+    begin = true;
   }
   else{
     locked = true;
   }
 }
 
+
 function playing(x,y){
-  if(!locked){
+  
+  if(!locked && !finnish){
+  drawnLines.push({
+    mouseX: mouseX,
+    mouseY: mouseY,
+    pmouseX: pmouseX,
+    pmouseY: pmouseY,
+  });
+    
   let l1 = distToSegment(x, y, 461, 112, 521, 275);
   let l2 = distToSegment(x, y, 521, 275, 695, 286);
   let l3 = distToSegment(x, y, 695, 286, 557, 389);
@@ -110,6 +157,7 @@ function playing(x,y){
   let l10 = distToSegment(x, y, 399, 273, 461, 112);
   
   if(int(l1) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -118,6 +166,7 @@ function playing(x,y){
   });
   }
     else if(int(l2) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -126,6 +175,7 @@ function playing(x,y){
   });
   }
     else if(int(l3) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -134,6 +184,7 @@ function playing(x,y){
   });
   }
     else if(int(l4) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -142,6 +193,7 @@ function playing(x,y){
   });
   } 
     else if(int(l5) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -150,6 +202,7 @@ function playing(x,y){
   });
   } 
     else if(int(l6) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -158,6 +211,7 @@ function playing(x,y){
   });
   } 
     else if(int(l7) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -166,6 +220,7 @@ function playing(x,y){
   });
   } 
     else if(int(l8) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -174,6 +229,7 @@ function playing(x,y){
   });
   } 
     else if(int(l9) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -182,6 +238,7 @@ function playing(x,y){
   });
   } 
     else if(int(l10) <= 10){
+    mood = true;
     cLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -190,6 +247,7 @@ function playing(x,y){
   });
   } 
   else{
+    mood = false;
     wLines.push({
     mouseX: mouseX,
     mouseY: mouseY,
@@ -202,17 +260,67 @@ function playing(x,y){
 }
 
 function end(){
+  if(drawnLines.length >10){
+    let start = drawnLines[0];
+    let end = drawnLines[drawnLines.length - 1];
   
+    if(start.mouseX == end.mouseX && start.mouseY == end.mouseY){
+      finnish = true;
+    }
+    if(start.mouseX >= 0.99*end.mouseX && start.mouseY >= 0.99*end.mouseY){
+      finnish = true;
+    }
+  }
 }
 
-//score function
+function endScreen(){
+  let message = "";
+  good.stop();
+  bad.stop();
+    if(score > 0){
+    message = "  CONGRATULATIONS!\n      YOU'RE A STAR!\n        Click On 'retry'\n         To Play Again";
+    stroke(5);
+    stroke("rgb(111,119,128)");
+    fill("rgb(225,234,234)");
+    rect(165, 145, 600, 400);
+    textSize(55);
+    stroke("rgb(29,29,29)");
+    fill("white");
+    text(message, 155, 255);
+    }
+    else{
+    message = "   Don't give up!\n   Click on 'retry' \n  to test your skill\n         again!";
+    stroke(5);
+    stroke("rgb(111,119,128)");
+    fill("rgb(213,213,231)");
+    rect(165, 145, 600, 400);
+    textSize(70);
+    stroke("rgb(29,29,29)");
+    fill("white");
+    text(message, 185, 245);
+    }
+}
+
 function Score(){
+  
+    let accuracy = cLines.length/drawnLines.length;
+    if(finnish && accuracy > 0.7 && drawnLines.length > 470){
+    if(score <= 50){
+      score = 20;
+      if(accuracy >= 0.8 &&cLines.length > 720){
+        score = 50;
+      }
+    }
+  }
+  
   let scr = score;
   textSize(40);
   fill("white");
   stroke("green");
   strokeWeight(8);
   text(scr, 363, 693);
+  
+  
 }
 
 //Attempt function
@@ -262,9 +370,7 @@ function setLineDash(list) {
   drawingContext.setLineDash(list);
 }
 
-function gotolink_retry(){
-  window.open("https://editor.p5js.org/camelia02/full/98A_LKVnm");
-}
+
 
 function gotolink_menu(){
   window.open('https://editor.p5js.org/manas__1404/full/48Gid6pnb')
@@ -272,4 +378,8 @@ function gotolink_menu(){
 
 function gotolink_info(){
   window.open("https://editor.p5js.org/manas__1404/full/aXWsq0nJz")
+}
+
+function gotolink_tracing(){
+  window.location.reload("https://editor.p5js.org/camelia02/full/98A_LKVnm");
 }
